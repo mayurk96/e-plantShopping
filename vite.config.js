fdf8b43b5b35e3de8ed1361edbfc83b05e4ff286
mayurk96 <173041171+mayurk96@users.noreply.github.com>
2025-03-14 02:24:02 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  base: "/shoppingreact",
+
   plugins: [react()],
-})
+  base: '/e-plantShopping/', // Make sure this matches your repo name
+  
+});
